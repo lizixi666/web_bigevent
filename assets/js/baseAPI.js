d8658d5638b34f790522ffe4f6ef858dd95cde3b
lizixi <lizixi666@163.com>
2022-05-03 11:28:38 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,7 @@
 //注意：每次调用$.get $.post $.ajax时候，会先调用ajaxprefilter这个函数
 //在这个函数中，可以拿到我们给ajax提供的配置对象
 $.ajaxPrefilter(function(options){
-    console.log(options.url);
+    // console.log(options.url);
     // 在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007'+options.url
     // console.log(options.url);
@@ -14,8 +14,8 @@ $.ajaxPrefilter(function(options){
 }
 //全局统一挂载complete回调函数
 options.complete = function(res){
-    console.log('执行了 complete回调');
-            console.log(res);
+    // console.log('执行了 complete回调');
+            // console.log(res);
             //在complete回调函数中,可以使用res.reponseJSON拿到服务器响应回来的数据
             if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
                 //1.强制清空token
